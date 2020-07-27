efc683fe0ced0cfb43870e8548e4fd22b6dd9b2f
llamaking136 <67032575+llamaking136@users.noreply.github.com>
2020-07-27 08:46:05 -0600

    Update header.hpp

--- header/header.hpp
+++ header/header.hpp
@@ -9,6 +9,11 @@ using namespace std;
 
 namespace header {
 
+    const string stdlib[1] = {"PRINT"};
+    const string stderrs[3] = {
+        "func not found", "data not found", "file not found"
+    };
+
     bool searchFile(const char *file) {
         ifstream infile(file);
         if (!infile.good()) {
@@ -18,8 +23,19 @@ namespace header {
         }
     }
 
-    void compileFile(const char *path) {
-        cout << path << endl;
+    string getToken(const string codeline) {
+        istringstream iss(codeline);
+        string token;
+        iss >> token;
+        return token;
+    }
+
+    bool compileFile(const char *path) {
+        if (searchFile(path)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     void runFile(string path) {
